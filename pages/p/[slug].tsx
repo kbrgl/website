48b4b2142b269c9b8f4827f52178ee31aa8ef361
Kabir Goel <kabirgoel.kg@gmail.com>
2020-11-21 20:41:43 +0530

    Improve SEO, edit blog posts, and fix Typescript (#12)
    * Remove "A Fresh New Look, and What Comes Now"
    * Add "Why are we so bad at introducing ourselves?"
    * Make blog post <title> tags more descriptive
    * Move blog posts to /p/ from /notes/

--- pages/p/[slug].tsx
+++ pages/p/[slug].tsx
@@ -1,4 +1,6 @@
-import fs from "fs";
+import Head from "next/head";
+// eslint-disable-next-line import/no-unresolved
+import fs from "fs/promises";
 import path from "path";
 import matter from "gray-matter";
 import remark from "remark";
@@ -6,50 +8,71 @@ import remarkHtml from "remark-html";
 import retextSmartypants from "retext-smartypants";
 import remarkFrontmatter from "remark-frontmatter";
 import remarkFootnotes from "remark-footnotes";
-import readingTime from "reading-time";
+import calculateReadingTime from "reading-time";
 import formatDate from "../../utils/format-date";
 import Layout from "../../components/layout";
 import Container from "../../components/container";
 
 import styles from "../../styles/Note.module.css";
 
-export default function Note({ title, date, html, readingTime }) {
+export default function Note({
+  title,
+  subtitle,
+  date,
+  html,
+  slug,
+  canonical,
+  readingTime,
+}) {
   return (
     <Layout>
       <Container>
-        <h1>{title}</h1>
+        <Head>
+          <title>{title} · Kabir Goel</title>
+          <meta name="description" content={subtitle} />
+          <meta property="og:title" content={title} />
+          <meta property="og:description" content={subtitle} />
+          <meta property="og:image" content="https://kabirgoel.com/me.png" />
+          <meta property="og:url" content={`https://kabirgoel.com/p/${slug}`} />
+          <meta property="og:type" content="blog" />
+          <meta name="twitter:card" content="summary_large_image" />
+          {canonical ? <link rel="canonical" href={canonical} /> : null}
+        </Head>
+        <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.subtitle}>{subtitle}</h2>
         <p className={styles.meta}>
-          {formatDate(new Date(date))} · {readingTime.text}
+          {formatDate(new Date(date))} · {readingTime}
         </p>
         <div
           className={styles.content}
+          // eslint-disable-next-line react/no-danger
           dangerouslySetInnerHTML={{ __html: html }}
-        ></div>
+        />
       </Container>
     </Layout>
   );
 }
 
-export async function getStaticProps({ params }) {
-  const filename = `${params.slug}.md`;
+export async function getStaticProps({ params: { slug } }) {
+  const filename = `${slug}.md`;
   const postsDirectory = path.join(process.cwd(), "content", "notes");
   const filePath = path.join(postsDirectory, filename);
-  const fileContents = fs.readFileSync(filePath, "utf8");
+  const fileContents = await fs.readFile(filePath, "utf8");
   const frontmatter = matter(fileContents).data;
   const processor = remark()
     .use(remarkFrontmatter)
     .use(retextSmartypants)
     .use(remarkFootnotes)
     .use(remarkHtml);
-  const file = await processor.processSync(fileContents);
+  const file = processor.processSync(fileContents);
 
   const html = String(file);
   const note = {
     ...frontmatter,
-    slug: path.basename(filename, path.extname(filename)),
+    slug,
     date: String(frontmatter.date),
     html,
-    readingTime: readingTime(html),
+    readingTime: calculateReadingTime(html).text,
   };
 
   return {
@@ -61,7 +84,7 @@ export async function getStaticProps({ params }) {
 
 export async function getStaticPaths() {
   const postsDirectory = path.join(process.cwd(), "content", "notes");
-  const filenames = await fs.readdirSync(postsDirectory);
+  const filenames = await fs.readdir(postsDirectory, "utf8");
 
   return {
     paths: filenames.map((filename) => ({
