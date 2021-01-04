54202f9fd9adf05fe1f496881f45fb95bfe26d26
Kabir Goel <kabirgoel.kg@gmail.com>
2021-01-04 12:30:08 +0530

    Add Simple Analytics with custom subdomain (#27)

---
+++ pages/_document.js
@@ -0,0 +1,30 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <script async defer src="https://a7s.kabirgoel.com/latest.js" />
+          <noscript>
+            <img
+              src="https://a7s.kabirgoel.com/noscript.gif"
+              alt=""
+              referrerpolicy="no-referrer-when-downgrade"
+            />
+          </noscript>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
