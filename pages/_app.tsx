ca3aa583433ee4e6129667723da2cea13e445bc8
Kabir Goel <kabirgoel.kg@gmail.com>
2021-01-18 12:16:41 +0530

    Move page title to _app.js

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,18 @@
 import "../styles/globals.css";
 import "../styles/prism-theme.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Kabir Goel</title>
+      </Head>
+      {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 // Only uncomment this method if you have blocking data requirements for
