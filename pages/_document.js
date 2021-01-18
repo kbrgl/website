b6cdfb418e1bdd064c3a694588f4a7e9ad844a92
Kabir Goel <kabirgoel.kg@gmail.com>
2021-01-19 01:07:36 +0530

    Optimize CSS loading

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,15 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <link rel="stylesheet" href="https://use.typekit.net/vyk7kso.css" />
+          <link
+            rel="preload"
+            href="https://use.typekit.net/vyk7kso.css"
+            as="style"
+            onLoad="this.onload=null;this.rel='stylesheet'"
+          />
+          <noscript>
+            <link rel="stylesheet" href="https://use.typekit.net/vyk7kso.css" />
+          </noscript>
         </Head>
         <body>
           <Main />
