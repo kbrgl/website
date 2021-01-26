7398f2de1fb434875a631c2e00a6e2964e3ba7fd
Kabir Goel <kabirgoel.kg@gmail.com>
2021-01-26 18:28:00 +0530

    Make footer stick to bottom when content is too small

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,10 +1,11 @@
 import Head from "next/head";
 import Navbar from "./navbar";
 import Footer from "./footer";
+import styles from "./layout.module.css";
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <div className={styles.layout}>
       <Navbar />
       <main>{children}</main>
       <Footer />
