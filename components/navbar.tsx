195fed8b8713a16e5d70e63abb6cb38cfcd40b05
Kabir Goel <kabirgoel.kg@gmail.com>
2021-03-14 05:27:30 +0530

    Add blog posts and about page and update image

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -13,6 +13,9 @@ export default function Navbar() {
             </Link>
           </div>
           <div className={styles.list}>
+            <Link href="/about">
+              <a>About</a>
+            </Link>
             <a href="mailto:kabirgoel.kg@gmail.com">Email</a>
             <a href="https://www.notion.so/kabirgoel/2b3224f21629468580551a14edae4616?v=5a2db9f0bb944564ae019709de328801">
               Playlist
