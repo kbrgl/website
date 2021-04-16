c9c4d84e837d0aa3721efad8a5814b3d10e9af83
Kabir Goel <kabirgoel.kg@gmail.com>
2021-04-16 13:04:47 +0530

    Fix portfolio image sizing on Safari

--- components/project.tsx
+++ components/project.tsx
@@ -6,18 +6,17 @@ export default function Project({ project }) {
 
   return (
     <Link href={link || `/p/${slug}`}>
-      <a className={styles.projectLink}>
+      <a className={styles.link}>
         <div className={styles.project}>
           <img
-            className={styles.projectImage}
+            className={styles.image}
             src={`/projects/${image}`}
             alt=""
             width={175}
-            height="auto"
           />
           <p>
-            <span className={styles.projectName}>{name}</span>
-            <span className={styles.projectDescription}>{description}</span>
+            <span className={styles.name}>{name}</span>
+            <span className={styles.description}>{description}</span>
           </p>
         </div>
       </a>
