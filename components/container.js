34def9d21992d1ae4aa194f7c1026593567752fd
Kabir Goel <kabirgoel@berkeley.edu>
2021-12-08 17:41:58 -0800

    Add className prop to container

--- components/container.js
+++ components/container.js
@@ -1,3 +1,5 @@
-export default function Container({ children }) {
-  return <div className="container px-10 max-w-2xl">{children}</div>;
+export default function Container({ children, className = "" }) {
+  return (
+    <div className={`container px-10 max-w-2xl ${className}`}>{children}</div>
+  );
 }
