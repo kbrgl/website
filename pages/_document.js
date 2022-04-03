037dad6cfd7038fc770236a953483235854a8a9f
Kabir Goel <kabirgoel.kg@gmail.com>
2022-04-03 08:36:32 -0700

    Remove Google Fonts links from _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,19 +11,7 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&display=swap"
-            rel="stylesheet"
-          />
           <link rel="stylesheet" href="https://use.typekit.net/vyk7kso.css" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;700&family=DM+Serif+Display&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
