e7f1d4e298694de0ca7d597678543463e88a6b3a
Kabir Goel <kabirgoel.kg@gmail.com>
2022-06-07 14:52:49 -0700

    Remove constrained layout

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,7 @@
 export default function Layout({ children }) {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-300">
-      <main className="flex-1 md:py-5">
-        <div className="mx-auto max-w-3xl bg-white md:border border-black">
-          {children}
-        </div>
-      </main>
+    <div className="min-h-screen flex flex-col">
+      <main className="flex-1">{children}</main>
     </div>
   );
 }
