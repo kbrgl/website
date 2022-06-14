d90df2d3ec506746b47c2c654a29ee9e982c80bf
Kabir Goel <kabirgoel.kg@gmail.com>
2022-06-14 02:23:29 -0700

    Update section heading appearance

--- src/components/section-heading.js
+++ src/components/section-heading.js
@@ -1,6 +1,6 @@
 export default function SectionHeading({ children }) {
   return (
-    <h2 className="text-lg font-bold flex items-center space-x-5">
+    <h2 className="text-xl font-serif font-bold flex items-center space-x-5 uppercase tracking-widest">
       <span>{children}</span>
       <span className="border-t flex-1" />
     </h2>
