4a82eb9a92ec6dfab7e027ea105d0851dae05a84
Kabir Goel <kabirgoel.kg@gmail.com>
2022-06-14 02:30:15 -0700

    Loosen tracking of section heading

--- src/components/section-heading.js
+++ src/components/section-heading.js
@@ -1,6 +1,6 @@
 export default function SectionHeading({ children }) {
   return (
-    <h2 className="text-xl font-serif font-bold flex items-center space-x-5 uppercase tracking-widest">
+    <h2 className="text-xl font-serif font-bold flex items-center space-x-5 uppercase">
       <span>{children}</span>
       <span className="border-t flex-1" />
     </h2>
