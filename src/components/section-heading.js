957d838d8c6660f9816236407861e0b564d0722e
Kabir Goel <kabirgoel.kg@gmail.com>
2022-06-14 02:21:06 -0700

    Group projects by date

--- src/components/section-heading.js
+++ src/components/section-heading.js
@@ -1,3 +1,8 @@
 export default function SectionHeading({ children }) {
-  return <h2 className="text-lg font-medium">{children}</h2>;
+  return (
+    <h2 className="text-lg font-bold flex items-center space-x-5">
+      <span>{children}</span>
+      <span className="border-t flex-1" />
+    </h2>
+  );
 }
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,29 +1,30 @@
+import _ from "lodash";
 import Layout from "../components/layout";
 import Container from "../components/container";
 import Project from "../components/project";
 import Header from "../components/header";
 import Title from "../components/title";
 import Footer from "../components/footer";
+import SectionHeading from "../components/section-heading";
 import parseDate from "../utils/parse-date";
-
 import projects from "../../content/projects.json";
 
-export default function Projects({ projects }) {
+export default function Projects({ projectsByYear, years }) {
   return (
     <Layout>
       <Header />
       <Container>
-        <p className="mt-12 mb-5 text-sm uppercase font-bold text-gray-500 tracking-widest">
-          Projects
-        </p>
-        <Title className="mb-12">
-          My favorite projects, including hacks, libraries, and experiments.
-        </Title>
-        <div className="gap-5 grid auto-rows-max grid-cols-1">
-          {projects.map((project) => (
-            <Project key={project.name} project={project} />
-          ))}
-        </div>
+        <Title className="my-24">Projects</Title>
+        {years.map((year) => (
+          <section className="mb-24 space-y-6" key={year}>
+            <SectionHeading>{year}</SectionHeading>
+            <div className="gap-8 grid auto-rows-max grid-cols-1">
+              {projectsByYear[year].map((project) => (
+                <Project key={project.name} project={project} />
+              ))}
+            </div>
+          </section>
+        ))}
         <div className="pt-10" />
       </Container>
       <Footer />
@@ -32,9 +33,15 @@ export default function Projects({ projects }) {
 }
 
 export function getStaticProps() {
+  const projectsByYear = _.groupBy(
+    projects.sort((a, b) => parseDate(b.date) - parseDate(a.date)),
+    (project) => parseDate(project.date).getUTCFullYear()
+  );
+  const years = Object.keys(projectsByYear).sort((a, b) => b - a);
   return {
     props: {
-      projects: projects.sort((a, b) => parseDate(b.date) - parseDate(a.date)),
+      projectsByYear,
+      years,
     },
   };
 }
