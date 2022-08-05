3c4184e390e66a11f04462382d3bbe6ee758114b
Kabir Goel <kabirgoel.kg@gmail.com>
2022-08-05 10:16:28 +0530

    fix: remove tutorial code

--- public/app.js
+++ public/app.js
@@ -8,12 +8,3 @@ if (
 } else {
   document.documentElement.classList.remove("dark")
 }
-
-// Whenever the user explicitly chooses light mode
-localStorage.theme = "light"
-
-// Whenever the user explicitly chooses dark mode
-localStorage.theme = "dark"
-
-// Whenever the user explicitly chooses to respect the OS preference
-localStorage.removeItem("theme")
