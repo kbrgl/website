f8abe93d0407d2789783193c4d2745b020611033
Kabir Goel <kabirgoel.kg@gmail.com>
2024-12-09 03:56:27 -0800

    Update bio

--- app/page.tsx
+++ app/page.tsx
@@ -7,30 +7,12 @@ export default async function Home() {
 
 	return (
 		<>
-			<section className="mt-24 mb-8 grid gap-4">
-				<h1 className="text-4xl font-medium font-title dark:text-white">
-					Designing, building, and refining.
+			<section className="mt-24 mb-8 grid gap-2">
+				<h1 className="text-4xl font-medium font-title dark:text-white max-w-xl">
+					Hey, I'm Kabir! I live in San Francisco, where I engineer products at{" "}
+					<a href="https://cartesia.ai">Cartesia</a>.
 				</h1>
-				<p className="markdown">
-					I engineer products at <a href="https://cartesia.ai">Cartesia</a> in
-					San Francisco.
-				</p>
-			</section>
-
-			<Subscribe />
-			<p className="mt-1.5 text-xs text-gray-500 dark:text-zinc-400">
-				Or follow on{" "}
-				<a href="https://twitter.com/KabirGoel" className="border-b">
-					Twitter
-				</a>
-			</p>
-
-			<section className="my-24">
-				<h2 className="flex items-center space-x-4 mb-4">
-					<span className="font-semibold">Recently</span>
-					<span className="h-px bg-gradient-to-r from-transparent to-gray-200 dark:to-zinc-700 flex-1" />
-				</h2>
-				<div className="text-gray-600 dark:text-gray-300 markdown">
+				<div className="text-gray-600 dark:text-gray-300 markdown max-w-xl">
 					<p>
 						I recently graduated from UC Berkeley, where I earned a degree in
 						Computer Science with Honors and a minor in Public Policy.
@@ -47,6 +29,14 @@ export default async function Home() {
 					</p>
 				</div>
 			</section>
+
+			<Subscribe />
+			<p className="mt-1.5 text-xs text-gray-500 dark:text-zinc-400">
+				Or follow on{" "}
+				<a href="https://twitter.com/KabirGoel" className="border-b">
+					Twitter
+				</a>
+			</p>
 
 			<div className="space-y-12 my-24">
 				{postsByYear
