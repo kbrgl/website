5f8c1ce3de14c7448da6e479a24491dbb682e659
Kabir Goel <kabirgoel.kg@gmail.com>
2025-03-09 21:37:16 -0700

    Fix biome lint on self-closing tags

--- components/cells.tsx
+++ components/cells.tsx
@@ -1,22 +1,22 @@
 export default function Cells() {
 	return (
 		<div className="absolute w-full grid grid-cols-4 auto-cols-fr gap-px bg-gradient-to-b from-gray-300 dark:from-zinc-700 to-transparent -z-10">
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
-			<div className="h-8 bg-white dark:bg-zinc-900"></div>
+			<div className="h-8 bg-white dark:bg-zinc-900" />
+			<div className="h-8 bg-white dark:bg-zinc-900" />
+			<div className="h-8 bg-white dark:bg-zinc-900" />
+			<div className="h-8 bg-white dark:bg-zinc-900" />
+			<div className="h-8 bg-white dark:bg-zinc-900" />
+			<div className="h-8 bg-white dark:bg-zinc-900" />
+			<div className="h-8 bg-white dark:bg-zinc-900" />
+			<div className="h-8 bg-white dark:bg-zinc-900" />
+			<div className="h-8 bg-white dark:bg-zinc-900" />
+			<div className="h-8 bg-white dark:bg-zinc-900" />
+			<div className="h-8 bg-white dark:bg-zinc-900" />
+			<div className="h-8 bg-white dark:bg-zinc-900" />
+			<div className="h-8 bg-white dark:bg-zinc-900" />
+			<div className="h-8 bg-white dark:bg-zinc-900" />
+			<div className="h-8 bg-white dark:bg-zinc-900" />
+			<div className="h-8 bg-white dark:bg-zinc-900" />
 		</div>
 	);
 }
